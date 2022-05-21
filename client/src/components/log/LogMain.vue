0323ea7f843f9b3614c8b58a8cbff7a942c90a56
mojo9492 <mojo9492@outlook.com>
2022-05-21 12:37:16 -0700

    add undo delete post

--- client/src/components/log/LogMain.vue
+++ client/src/components/log/LogMain.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { Ref, ref, onMounted } from 'vue'
+import { Ref, ref, onMounted, watch } from 'vue'
 import { Post } from '@/models'
 import LogTable from './LogTable.vue'
 
@@ -9,20 +9,10 @@ interface ILogProps {
     userEmail: string
 }
 const props = defineProps<ILogProps>()
-
-defineEmits(['postToAdd', 'postToDelete'])
-
+// * refs
 const userEntries: Ref<Post[]> = ref([])
-
-onMounted(async () => {
-    const response = await fetch('http://localhost:3000/post/' + props.userId)
-    if (!response.ok) {
-        throw new Error('could not find posts: ' + props.userEmail)
-    }
-    const data = await response.json()
-    userEntries.value = data
-})
-
+const showUndo = ref(false)
+const deletedPost: Ref<Post | undefined> = ref()
 const handleAddPost = async (post: Post) => {
     const response = await fetch('http://localhost:3000/post', {
         method: 'POST',
@@ -37,7 +27,6 @@ const handleAddPost = async (post: Post) => {
     }
     userEntries.value.push(post)
 }
-
 const handleDelete = async (id: string) => {
     const deleteResult = await fetch(`http://localhost:3000/post/${id}`, {
         method: 'DELETE',
@@ -47,18 +36,52 @@ const handleDelete = async (id: string) => {
         throw new Error('could not delete post: ' + id)
     }
     // todo: handle undo delete
+    showUndo.value = true
+    deletedPost.value = userEntries.value.find((post) => post.id === id)
     // * returns the deleted post for undo purposes
     await deleteResult.json()
     // * remove the deleted post from the UI
     userEntries.value = userEntries.value.filter(post => post.id !== id)
 }
+const undoDeletePost = async () => {
+    const response = await fetch('http://localhost:3000/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(deletedPost.value),
+    })
+    if (!response.ok) {
+        const data = await response.json()
+        throw new Error('could not add post: ' + data.body)
+    }
+    userEntries.value.push(deletedPost.value as Post)
+    showUndo.value = false
+}
+defineEmits(['postToAdd', 'postToDelete'])
+onMounted(async () => {
+    const response = await fetch('http://localhost:3000/post/' + props.userId)
+    if (!response.ok) {
+        throw new Error('could not find posts: ' + props.userEmail)
+    }
+    const data = await response.json()
+    userEntries.value = data
+})
+watch(showUndo, () => {
+    setTimeout(() => {
+        showUndo.value = false
+        deletedPost.value = undefined
+    }, 8000)
+})
 </script>
 
 <template>
     <div id="container">
+        <div v-if="showUndo"><button class="undo-button" @click="undoDeletePost">Undo?</button></div>
         <h1 v-if="userEntries">Welcome, {{ props.userName }}</h1>
         <h1 v-else>Welcome back</h1>
-        <LogTable @postToAdd="handleAddPost" @postToDelete="handleDelete" :author-id="props.userId" :author-first="props.userName" :entries="userEntries" />
+        <LogTable @postToAdd="handleAddPost" @postToDelete="handleDelete" :author-id="props.userId"
+            :author-first="props.userName" :entries="userEntries" />
     </div>
 </template>
 
@@ -66,4 +89,17 @@ const handleDelete = async (id: string) => {
     #container
         display: flex
         flex-flow: column nowrap
+
+        .undo-button
+            background: red
+            color: white
+            border: none
+            border-radius: .25em
+            height: 3em
+            width: 6em
+            cursor: pointer
+            &:hover
+                background: red
+            &:active
+                background: red
 </style>
