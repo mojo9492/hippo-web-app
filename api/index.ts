3ba3f42f7d07bdca16eacfcd3520f89253571cfd
mojo9492 <mojo9492@outlook.com>
2022-05-29 12:28:57 -0700

    add dotenv cli to configure .env.dev with prisma for local environments

--- api/index.ts
+++ api/index.ts
@@ -12,28 +12,27 @@ import getPostsByUserId from './routes/getPostsByUserId'
 import path from 'path'
 import home from './routes/home'
 
+export const staticPath = path.join(__dirname, 'public/views')
+
 const app = express()
 app.use(helmet())
 app.use(express.json())
 app.use(cors())
-
-export const staticPath = path.join(__dirname, 'public/views')
-
 app.use(express.static(staticPath, { index: false }))
-
+// * get
 app.get('/', home)
 app.get('/api/v1/users', getUsers) // * admin, will delete in prod
 app.get('/api/v1/feed', getPosts) // * admin, will delete in prod
 app.get('/api/v1/user/:email', getUserByEmail) // * admin will be replaced by login JWT
 app.get('/api/v1/post/:id', getPostsByUserId) // * admin will be replaced by login JWT
-
+// * post
 app.post('/api/v1/user', postUser)
 app.post('/api/v1/post', postPost)
-
+// * put
 app.put('/api/v1/post/publish/:id', putPost)
-
+// * delete
 app.delete('/api/v1/post/:id', deletePost)
 
 app.listen(3000, () =>
-    console.log('API server ready at: http://localhost:3000')
+    console.log(`env: ${process.env.NODE_ENV} API server ready at: http://localhost:3000`)
 )
